fe166fc9305bbb33a88d7e1a5b4331122dc96152
swsilver95 <dmstmddnjs@hanmail.net>
2021-07-21 01:16:53 +0900

    0720 Algorithm modified

--- algorithm/BJ1912_succedsum.py
+++ algorithm/BJ1912_succedsum.py
@@ -24,6 +24,20 @@ import sys
 n = sys.stdin.readline()
 numbers = list(map(int, sys.stdin.readline().split(" ")))
 
+my_list = [numbers[0]] # max로 정답을 출력할 리스트, for 연산을 1부터 시작할 것이므로 [0]값을 미리 할당
+tmp = numbers[0] # 리스트에서 직접적으로 더하거나 비교할 값
+
+for number in numbers[1:]: # 첫 번째 값을 제외한 list 내 항목들에 대해
+    if tmp < 0: # tmp가 음수인 경우,
+        tmp = number # 더이상 tmp에 데이터를 더할 필요가 없으므로 tmp 값을 number로 바꿔버림
+    else: # 그런데 tmp가 음수는 아닌 경우
+        tmp = tmp + number # 일단 더해봄
+
+    my_list.append(tmp) # tmp vs. numbers[0] 중에서 누가 큰지 모르니까 일단 리스트에 넣음 
+    
+print(max(my_list)) # my_list에서 가장 큰 값을 출력함
+
+
 '''
 sumlist = []
 stk = []
@@ -42,16 +56,3 @@ for i in range(len(data)):
 
 print(max(sumlist))
 '''
-
-stk = [numbers[0]]
-tmp = numbers[0]
-
-for number in numbers[1:]:
-    if tmp < 0:
-        tmp = number
-    else:
-        tmp = tmp + number
-
-    stk.append(tmp)
-    
-print(max(stk))
