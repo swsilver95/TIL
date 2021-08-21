fd29316bdb0dc5db3687459b6c184b89f8691fd2
swsilver95 <dmstmddnjs@hanmail.net>
2021-08-21 22:15:31 +0900

    0821 Algorithm

---
+++ algorithm/BOJ/BOJ2559_수열_S3.py
@@ -0,0 +1,25 @@
+import sys
+
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+temps = list(map(int, input().split()))
+
+
+def high_sum(numbers):
+    global N
+    global K
+    high = []
+    high_tmp = 0
+    for i in range(K):
+        high_tmp += numbers[i]
+
+    max_tmp = high_tmp
+    for i in range(K, N):
+        high_tmp = high_tmp - numbers[i - K] + numbers[i]
+        max_tmp = max(max_tmp, high_tmp)
+
+    return max_tmp
+
+
+print(high_sum(temps))
