cc4bb4df6e7be607a2c30873db4d6108b24dc3c1
silver <dmstmddnjs@gmail.com>
2022-01-31 18:41:42 +0900

    0131 Algorithm

---
+++ algorithm/BOJ/BOJ11047_동전0_S2.py
@@ -0,0 +1,12 @@
+N, K = map(int, input().split())
+coins = [int(input()) for _ in range(N)]
+coins.reverse()
+
+ans = 0
+for coin in coins:
+    ans += K // coin
+    K %= coin
+    if K == 0:
+        break
+
+print(ans)
