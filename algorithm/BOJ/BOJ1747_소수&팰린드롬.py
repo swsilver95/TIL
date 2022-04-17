b1c1f85f40a9c68b3f15ee2845b3231fa4e91033
swsilver95 <dmstmddnjs@hanmail.net>
2022-04-17 21:02:42 +0900

    0417 Algorithm

---
+++ algorithm/BOJ/BOJ1747_소수&팰린드롬.py
@@ -0,0 +1,29 @@
+N = int(input())
+n = 1003002
+
+check = [True] * n
+check[0] = False
+check[1] = False
+m = round(int(n ** 0.5))
+
+for i in range(2, m + 1):
+    if check[i]:
+        for j in range(i + i, n, i):
+            check[j] = False
+
+
+def is_pal(num):
+    tmp = str(num)
+    if tmp == tmp[::-1]:
+        return True
+    return False
+
+
+def solution(b):
+    for k in range(b, n):
+        if check[k] and is_pal(k):
+            print(k)
+            return
+
+
+solution(N)
